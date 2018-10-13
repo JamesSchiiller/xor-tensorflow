e6257cfac1ecd67e1f76fb8035ac53106f22af57
JamesSchiiller <jgschiiller@gmail.com>
2018-10-13 13:17:58 -0400

    simplified

--- xor.py
+++ xor.py
@@ -1,17 +1,14 @@
 from numpy import array
 from keras.models import Sequential
 from keras.layers import Dense
-# define dataset
+
 X = array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = array([0, 1, 1, 0])
-# define model
 model = Sequential()
 model.add(Dense(100, activation='relu', input_dim=2))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
-# fit model
 model.fit(X, y, epochs=2000, verbose=0)
-# demonstrate prediction
 x_input = array([1, 0])
 x_input = x_input.reshape((1, 2))
 yhat = model.predict(x_input, verbose=0)
